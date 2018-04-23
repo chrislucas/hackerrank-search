0825999a954dc5b5a449de0459f02cc7e89b331e
Christoffer Lucas <christoffer.luccas@gmail.com>
2018-04-23 01:27:08 -0300

    estudos sobre BIT and Segment Tree

--- python/segment_tree/trees/SegmentTree.py
+++ python/segment_tree/trees/SegmentTree.py
@@ -1,4 +1,4 @@
-from math import log2
+from math import log2, ceil
 
 
 class SegmentTree:
@@ -6,12 +6,12 @@ class SegmentTree:
     def __init__(self, data):
         self.data = data
         self.len_data = len(data)
-        self.height_tree = log2(len(data))
+        self.height_tree = ceil(log2(len(data)))
         self.mem_size = int(2 * 2 ** self.height_tree - 1)
         self.segment_tree = [0] * self.mem_size
-        self.build_tree(0, self.len_data-1, 0)
+        self._build_tree(0, self.len_data-1, 0)
 
-    def get_middle(self, s, e):
+    def _get_middle(self, s, e):
         return (e - s) // 2 + s
 
     '''
@@ -21,21 +21,20 @@ class SegmentTree:
         :param cur_index = indice da segment_tree. Usamos essa valor
         para definir qual o valor do n-esino nó da Segment Tree
     '''
-    def build_tree(self, s, e, cur_index):
+    def _build_tree(self, s, e, cur_index):
         if s == e:
             self.segment_tree[cur_index] = self.data[e]
             return self.data[e]
-        middle_index = self.get_middle(s, e)
+        middle_index = self._get_middle(s, e)
         '''
         usamos um array para representar a Segment Tree
         dado um indice i, _tree[i] eh o no raiz e _tree[i*2+1] eh o
         no da esquerda e _tree[i*2+2] o no da direita
         '''
-        l_value = self.build_tree(s, middle_index, cur_index * 2 + 1)
-        r_value = self.build_tree(middle_index + 1, e, cur_index *2 + 2)
+        l_value = self._build_tree(s, middle_index, cur_index * 2 + 1)
+        r_value = self._build_tree(middle_index + 1, e, cur_index * 2 + 2)
         self.segment_tree[cur_index] = l_value + r_value
         return self.segment_tree[cur_index]
-
 
     '''
         :parameter index: indice que se quer atualizar no array 'data'
@@ -45,20 +44,18 @@ class SegmentTree:
         if index < 0 or index > self.len_data - 1:
             return
         # valor que precisa ser adicionado aos elementos da arvore de segmento
-        diff = self.data[index] - value
+        diff = value - self.data[index]
         self.data[index] = value
         return self._update(0, self.len_data - 1, index, diff, 0)
 
     def _update(self, li, ls, index, value, cur_index):
         if index < li or index > ls:
             return
-
         self.segment_tree[cur_index] += value
         if li != ls:
-            middle_index = self.get_middle(li, ls)
+            middle_index = self._get_middle(li, ls)
             self._update(li, middle_index, index, value, cur_index * 2 + 1)
             self._update(middle_index+1, ls, index, value, cur_index * 2 + 2)
-
         return
 
     '''
@@ -67,18 +64,18 @@ class SegmentTree:
         : i e j sao os limites que se quer saber a soma
     '''
     def _prefix_sum(self, li, ls, i, j, cur_index):
-        if i > ls or j < li or i > j:
+        if i > ls or j < li:
             return 0
         elif i <= li and j >= ls:
             return self.segment_tree[cur_index]
-        middle_index = self.get_middle(i, j)
+        middle_index = self._get_middle(li, ls)
         l_value = self._prefix_sum(li, middle_index, i, j, cur_index * 2 + 1)
         r_value = self._prefix_sum(middle_index + 1, ls, i, j, cur_index * 2 + 2)
         return l_value + r_value
 
     def prefix_sum(self, i, j):
-        if i < 0 or j > self.len_data - 1:
-            return 0
+        if i < 0 or j > self.len_data - 1 or i > j:
+            return -1
         return self._prefix_sum(0, self.len_data - 1, i, j, 0)
 
 
@@ -91,8 +88,10 @@ st = SegmentTree(matrix[idx])
 
 print(st.prefix_sum(0, 3))
 print(st.prefix_sum(1, 3))
-print(st.update(1, 10))
-#print(st.update(0, 10))
+st.update(1, 10)
+print(st.prefix_sum(0, 3))
+st.update(1, 0)
+print(st.prefix_sum(0, 3))
 
 if __name__ == '__main__':
     pass
